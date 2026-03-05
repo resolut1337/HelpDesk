ad8e530c68acf2d922acc55ab8e7ff5584dd7f69
Fishchenko <oleksandrfi17@gmail.com>
2026-03-05 21:28:35 +0200

    Private Routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AppStateProvider } from './context/AppStateContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppStateProvider>
-      <App />
-    </AppStateProvider>
+    <BrowserRouter>
+      <AppStateProvider>
+        <App />
+      </AppStateProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
